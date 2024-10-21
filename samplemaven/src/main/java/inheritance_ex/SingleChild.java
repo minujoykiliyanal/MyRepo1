2319145285e4b8b8e9124f8327a80495386c7f6f
minuj <minuj@MINU_JOY>
2024-10-21 12:37:50 +0530

    Done Single Inheritance Programme

---
+++ samplemaven/src/main/java/inheritance_ex/SingleChild.java
@@ -0,0 +1,18 @@
+package inheritance_ex;
+
+public class SingleChild extends SingleParent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingleChild obj=new SingleChild();
+		obj.display();
+		obj.display2();
+
+	}
+public void display2() {
+System.out.println("Single Inherutance child class");	
+}
+	
+}
+
+
---
+++ samplemaven/src/main/java/inheritance_ex/SingleParent.java
@@ -0,0 +1,9 @@
+package inheritance_ex;
+
+public class SingleParent {
+	public void display() {
+		System.out.println("Single inheritance parent class");
+		
+	}
+
+}
